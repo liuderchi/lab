bbd850cbeb726d56329c60facaa01018404ab095
Te-Chi Liu <liuderchi@gmail.com>
2018-09-22 18:58:47 +0800

    POC: 💯 init markdown template
    - API ref: https://code.visualstudio.com/docs/extensionAPI/vscode-api#TextEditor

--- vscode-extension-hello-world/hello-world/src/extension.ts
+++ vscode-extension-hello-world/hello-world/src/extension.ts
@@ -2,6 +2,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+
+const getYearMonthDay = (date = new Date()) => ([
+    date.getFullYear(),
+    String(date.getMonth() + 1).padStart(2, "0"),
+    String(date.getDate()).padStart(2, "0"),
+]);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -39,8 +46,28 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage(`Selected Chars: ${text.length}`);
     });
 
+    let disposable3 = vscode.commands.registerCommand('extension.markdownInitTemplate', () => {
+        const filenameNoExt = /(.*?)(?:\.[^.]+)?$/.exec(
+            path.basename(vscode.window.activeTextEditor.document.fileName)
+        )[1];
+        const fileNameCap = filenameNoExt
+            .replace(/[!-.\:-\@\[-\`\{-~]/g, ' ')    // non-Word, /[\W_]/g
+            .split(' ')
+            .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+            .join(' ');
+        const [ year, month, day ] = getYearMonthDay();
+
+        const template = `# ${fileNameCap}\n\n## Date\n\n- ${year}-${month}-${day}\n\n## Description\n\n-\n__WIP ${year}-${month}-${day}__`
+
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+
+        editor.edit(edit => edit.insert(new vscode.Position(0,0), template));
+    });
+
     context.subscriptions.push(disposable);
     context.subscriptions.push(disposable2);
+    context.subscriptions.push(disposable3);
 }
 
 // this method is called when your extension is deactivated
