65dbfb3ccc544ea0a82a4f8e6940d6695b17b9d6
liuderchi <liuderchi@gmail.com>
2018-11-03 16:54:01 +0800

    add sanitize.js, validation.js

---
+++ boostnote-util/src/validation.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const argv = require('yargs')
+  .example(
+    '$ node ./src/validation.js -p ~/foo/bar/notes',
+    'validate notes in ~/foo/bar/notes'
+  )
+  .describe('path', 'specify folder contain notes')
+  .demandOption(
+    ['path'],
+    'Please provide path arguments to work with this tool'
+  )
+  .alias('path', ['p'])
+  .help('h').argv;
+
+const fs = require('fs');
+const path = require('path');
+const cson = require('cson');
+const { expect } = require('chai');
+
+const PROJ_ROOT = argv.path;
+const IGNORES = ['.git'];
+const notesIndex = {};
+
+describe('validation boostnote', () => {
+  describe('validate boostnote tags', () => {
+    const VALID_YEAR = ['2016', '2017', '2018', '2019', '2020', '2021'];
+    const csonPaths = fs
+      .readdirSync(PROJ_ROOT)
+      .filter(p => IGNORES.find(i => i !== p))
+      .map(p => path.join(PROJ_ROOT, p));
+
+    csonPaths.forEach(csonPath => {
+      let c = cson.load(csonPath);
+      it('has `20xx` year record as first tag', () => {
+        const errMsg = `\n\ttags of ${c.title} (${path.basename(
+          csonPath
+        )}): ${JSON.stringify(c.tags)}`;
+        expect(c.tags[0]).to.be.oneOf(VALID_YEAR, errMsg);
+      });
+    });
+  });
+});
