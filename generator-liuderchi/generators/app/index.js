9776f98513f9e63bc054732a76f69550081b2614
liuderchi <liuderchi@gmail.com>
2018-11-10 21:18:54 +0800

    add package.json in template
    - do not make bower warning, set yarn as installer
    - https://github.com/yeoman/generator/blob/93ad4e0d2757d4a35d8bb5b4d76fb065f3057952/lib/actions/install.js#L111

--- generator-liuderchi/generators/app/index.js
+++ generator-liuderchi/generators/app/index.js
@@ -32,9 +32,18 @@ module.exports = class extends Generator {
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
     );
+    this.fs.copy(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json')
+    );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      // http://yeoman.io/generator/Generator.html#installDependencies
+      bower: false,
+      npm: false,
+      yarn: true,
+    });
   }
 };
