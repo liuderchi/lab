d3bd1f543f5ad664a0be5ac9dd30a5615ce82445
liuderchi <liuderchi@gmail.com>
2018-11-12 01:29:09 +0800

    WIP: parse cil arg, options
    - refactor: WIP: parse cil arg, options

--- generator-liuderchi/generators/app/index.js
+++ generator-liuderchi/generators/app/index.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
@@ -12,29 +12,39 @@ module.exports = class extends Generator {
       )
     );
 
+    // Parse cli arg options
+    const {
+      options,
+      args: [name],
+    } = this;
+    const type = options.type || options.t;
+    console.warn({ name, type });
+
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true,
+        type: 'input',
+        name: 'name',
+        message: 'Package Name?',
+        default: name || 'my-package',
       },
       {
-        type: 'input',
-        name: 'fileName',
-        message: 'Example File Name?',
-        default: 'foobar',
+        type: 'list',
+        name: 'type',
+        message: 'Package Type?',
+        choices: [
+          'cli',
+          'lib',
+          // 'web'
+        ],
+        default: type || 'cli',
       },
     ];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.props = await this.prompt(prompts);
   }
 
   writing() {
-    const { fileName } = this.props;
+    const { name } = this.props;
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
@@ -46,9 +56,9 @@ module.exports = class extends Generator {
     // https://github.com/sboudrias/mem-fs-editor#copytplfrom-to-context-templateoptions--copyoptions
     this.fs.copyTpl(
       this.templatePath('dummyfileWithTpl.txt'),
-      this.destinationPath(`${fileName}.txt`),
+      this.destinationPath(`${name}.txt`),
       {
-        fileName,
+        name,
       }
     );
   }
