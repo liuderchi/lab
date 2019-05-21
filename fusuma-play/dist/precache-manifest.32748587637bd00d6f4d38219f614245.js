eb7dbe6cc7ff69fe5ae793fdf8eb75adee1c16c9
liuderchi <liuderchi@gmail.com>
2019-05-21 23:15:17 +0800

    deploy to sub folder

---
+++ fusuma-play/dist/precache-manifest.32748587637bd00d6f4d38219f614245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450a6d897b9091fe04911e34bd6feac1",
+    "url": "index.html"
+  },
+  {
+    "url": "main.1.b46947b7902cf441fa42.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.1e37ec8b16c663dd4f3e.bundle.js"
+  },
+  {
+    "url": "runtime.d86e7d41502c94362f6a.bundle.js"
+  },
+  {
+    "url": "target-blank.4.2610674694803ee1de9e.bundle.js"
+  },
+  {
+    "url": "vendor.5.367c36f896ec5059b452.bundle.js"
+  },
+  {
+    "url": "vendor.5.367c36f896ec5059b452.css"
+  },
+  {
+    "url": "vendors~Sidebar.0.a7df520438b9f49b645b.bundle.js"
+  },
+  {
+    "url": "vendors~Sidebar.0.a7df520438b9f49b645b.css"
+  }
+]);
