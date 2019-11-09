fce745af3c42e165950ef1652ec3e370b8df22a8
liuderchi <liuderchi@gmail.com>
2019-11-09 17:46:08 +0800

    prepare bash script, refactor

---
+++ puppeteer-base-project/script/commands.sh
@@ -0,0 +1,17 @@
+PPT_PROJ_ROOT="$HOME/lab/puppeteer-base-project"
+startNewChromeWindow() {
+  # local appPath='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  # local appPath='/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+  local appPath='/Applications/Chromium.app/Contents/MacOS/Chromium'
+  $appPath \
+    --remote-debugging-port=9222 \
+    --no-first-run \
+    --no-default-browser-check \
+    --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
+}
+parseJiraTitle() {
+  if [ -z "$1" ]; then echo 'should provide ticket number as args[1]'; return -1; fi
+  (cd $PPT_PROJ_ROOT && \
+      npm rum parse-jira-title -- $1
+  )
+}
