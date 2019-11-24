86be30596f65ba6ea9b1a287e7e445f8c1c2f225
liuderchi <liuderchi@gmail.com>
2019-11-24 21:31:47 +0800

    [1] from csb template

---
+++ my-react-app-template/.eslintrc.js
@@ -0,0 +1,43 @@
+// https://github.com/evenchange4/hsu-scripts/blob/master/eslint.js
+
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/flowtype',
+  ],
+  plugins: ['prettier', 'jest', 'flowtype', 'react-hooks'],
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-named-as-default': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['route'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+    'react/default-props-match-prop-types': [
+      'error',
+      { allowRequiredDefaults: true },
+    ],
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+  },
+};
