bf54d8df4bb630428923215adfe0faf1df021416
liuderchi <liuderchi@gmail.com>
2019-11-26 18:04:11 +0800

    WIP

--- puppeteer-base-project/script/commands.sh
+++ puppeteer-base-project/script/commands.sh
@@ -9,6 +9,16 @@ startNewChromeWindow() {
     --no-default-browser-check \
     --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
 }
+startChromeWithRemoteDubuggingPort() {
+  # local appPath='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  # local appPath='/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+  local appPath='/Applications/Chromium.app/Contents/MacOS/Chromium'
+  $appPath \
+    --remote-debugging-port=9222
+}
+oktaLogin() {
+  (cd $PPT_PROJ_ROOT && node script/oktaLogin.js)
+}
 parseJiraTitle() {
   if [ -z "$1" ]; then echo 'should provide ticket number as args[1]'; return -1; fi
   (cd $PPT_PROJ_ROOT && \
