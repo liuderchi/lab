9a85bac97804970308660e06a16ee784cf816a67
liuderchi <liuderchi@gmail.com>
2019-12-02 00:10:33 +0800

    WIP 2

--- puppeteer-base-project/script/oktaLogin.js
+++ puppeteer-base-project/script/oktaLogin.js
@@ -5,17 +5,15 @@ require('dotenv').config();
 // check .env
 const {
   CHROME_ENDPOINT,
-  HIMAIL_URL,
-  HIMAIL_ACCOUNT,
-  HIMAIL_PWD,
-  HIMAIL_INBOX_URL,
+  OKTA_URL,
+  OKTA_ACCOUNT,
+  OKTA_PSD
 } = process.env;
 if (
   !CHROME_ENDPOINT ||
-  !HIMAIL_URL ||
-  !HIMAIL_ACCOUNT ||
-  !HIMAIL_PWD ||
-  !HIMAIL_INBOX_URL
+  !OKTA_URL ||
+  !OKTA_ACCOUNT ||
+  !OKTA_PSD
 ) {
   console.error('Bad .env config, please check all required values');
   process.exit(-3);
@@ -33,49 +31,80 @@ const main = async () => {
   });
 
   const page = await browser.newPage();
-  const pageUrl = `${HIMAIL_URL}`;
+  const pageUrl = `${OKTA_URL}`;
   await page.goto(pageUrl, {
     waitUntil: 'networkidle0', // 'networkidle0' is very useful for SPAs.
   });
 
   try {
+    // account input
     await page.evaluate(
-      (accountValue, passwordValue) => {
-        const inputDoms = document.querySelectorAll('.ctrlGroup > input');
-        const accountDom = [...inputDoms].filter(el => el.id === 'idPuser')[0];
-        const passwordDom = [...inputDoms].filter(
-          el => el.id === 'passPuser',
-        )[0];
-        const submitDom = [...inputDoms].filter(el => el.id === 'OKPuser')[0];
+      () => {
+        const inputDoms = document.querySelectorAll('input');
+        const accountDom = [...inputDoms].filter(el => el.id === 'okta-signin-username')[0];
+        if (accountDom) {
+          accountDom.focus()
+        } else {
+          throw Error('input account failed');
+        }
+      },
+    );
+    await page.keyboard.type(OKTA_ACCOUNT);
 
-        if (accountDom && passwordDom && submitDom) {
-          console.log('will login');
-          accountDom.value = accountValue;
-          passwordDom.value = passwordValue;
-          submitDom.click();
+    // password input
+    await page.evaluate(
+      () => {
+        const inputDoms = document.querySelectorAll('input');
+        const passwordDom = [...inputDoms].filter(
+          el => el.id === 'okta-signin-password',
+        )[0];
+
+        if (passwordDom) {
+          passwordDom.focus()
         } else {
           throw Error('input password failed');
         }
       },
-      HIMAIL_ACCOUNT,
-      HIMAIL_PWD,
+    );
+    // await page.keyboard.press('Tab');
+    await page.keyboard.type(OKTA_PSD);
+
+    // submit button
+    await page.evaluate(
+      () => {
+        const inputDoms = document.querySelectorAll('input');
+        const submitDom = [...inputDoms].filter(el => el.id === 'okta-signin-submit')[0];
+
+        if (submitDom) {
+          submitDom.click();
+        } else {
+          throw Error('input password failed');
+        }
+      }
     );
 
-    // wait for inbox link dom
-    const inboxLinkQuery =
-      '.sidebar-folder-list > ul:nth-child(2) > li:first-child > a';
-    await page.waitFor(inboxLinkQuery);
+    // wait for push button
+    await page.waitForNavigation(4000, {
+      waitUntil: 'networkidle0'
+    })
+    if (page.url() !== 'https://oath.okta.com/signin/verify/duo/web') throw Error('url is not correct');
 
-    if (page.url() !== HIMAIL_INBOX_URL) throw Error('url is not correct');
+    // WIP
+
+    const pushButtonQuery = 'fieldset[data-device-index="phone1"] .row-label.push-label > button.auth-button.positive'
+    // await page.waitFor(pushButtonQuery, {visible: true});
 
     await page.evaluate(query => {
-      const inboxLinkDom = [...document.querySelectorAll(query)][0];
-      if (inboxLinkDom) {
-        inboxLinkDom.click();
+      // const pushButtonDom = [...document.querySelectorAll(query)][0];
+      const pushButtonDom = [...document.querySelectorAll('button')][0];
+      console.log({pushButtonDom})
+      if (pushButtonDom) {
+        // pushButtonDom.click();
       } else {
-        throw Error('could not find inbox link');
+        throw Error('could not click push button');
       }
-    }, inboxLinkQuery);
+    }, pushButtonQuery);
+
   } catch (error) {
     console.log('⚠️  Login Failed', error);
     process.exit(-1);
