7185a2349124eaea48ef744c465ada44b2d445ab
liuderchi <liuderchi@gmail.com>
2019-12-11 20:27:00 +0800

    render -> hydrate

--- react-server-boilerplate/src/app/browser.js
+++ react-server-boilerplate/src/app/browser.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import App from './index';
 
-render(<App {...window.__APP_INITIAL_STATE__} />, document.getElementById('root'));
+hydrate(<App {...window.__APP_INITIAL_STATE__} />, document.getElementById('root'));
 
 // render(<App />, document.getElementById('root'));
